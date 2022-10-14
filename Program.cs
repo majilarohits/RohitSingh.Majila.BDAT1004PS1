1773bf74f442235113e93ba0006f40cd65110114
majilarohits <115721796+majilarohits@users.noreply.github.com>
2022-10-14 19:22:07 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Countstring
+{
+    public static void Main()
+    {
+        String str1 = "Supercalifragilisticexpialidocious";
+        int count = 0;
+
+        for (int n = 0; n < str1.Length; n++)
+        {
+            if (str1[n] != ' ')
+                count++;
+        }
+
+        Console.WriteLine("Total number of characters in a string: " + count);
+    }
+}
+
