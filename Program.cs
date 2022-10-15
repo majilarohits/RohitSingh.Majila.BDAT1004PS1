5dd10755bd606a2d373adcef36c8561bf679e70b
majilarohits <115721796+majilarohits@users.noreply.github.com>
2022-10-14 22:16:01 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class longestword
+{
+    public static void Main()
+    {
+        string[] words = { "Supercalifragilisticexpialidocious", "Honorificabilitudinitatibus", "Bababadalgharaghtakamminarronnkonn" };
+
+        int longestLength = words.Max(w => w.Length);
+
+        Console.WriteLine("The longest word is {0} characters long.", longestLength);
+    }
+}
+
