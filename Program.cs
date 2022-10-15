b4494aae80423a7ccd03af02d8e0f458e52699df
majilarohits <115721796+majilarohits@users.noreply.github.com>
2022-10-14 22:14:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class substring
+{
+    static void Main(string[] args)
+    {
+        String str = "Supercalifragilisticexpialidocious";
+        String value = "ice";
+        Boolean result = str.Contains(value);
+        Console.WriteLine($"Does string contain specified substring? {result}");
+    }
+}
+
