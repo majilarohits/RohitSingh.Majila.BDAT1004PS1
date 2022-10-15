310611f7867177e0dec973922b3effd32e1144e4
majilarohits <115721796+majilarohits@users.noreply.github.com>
2022-10-14 22:17:32 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+
+{
+
+    static void Main()
+
+    {
+
+        string[] a = new string[]
+
+        {
+
+            "Berlioz",
+
+            "Borodin",
+
+            "Brian",
+
+            "Bartok",
+
+            "Bellini",
+
+            "Buxtehude",
+
+            "Bernstein",
+
+        };
+
+        Array.Sort(a);
+
+        foreach (string s in a)
+
+        {
+
+            Console.WriteLine(s);
+
+        }
+
+    }
+
+}
